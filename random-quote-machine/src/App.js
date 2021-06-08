b1b12ca930f4bf57c40109bf2baa9b71bb5c5e93
Nadatari <dariosting@gmail.com>
2021-06-08 16:19:38 +0200

    Finished all fCC requirements

--- random-quote-machine/src/App.js
+++ random-quote-machine/src/App.js
@@ -1,9 +1,14 @@
 
 function App() {
   return (
-    <div className="machine">
-      <h3>Hello</h3>
-    </div>
+    <main  class="container-fluid d-flex flex-column justify-content-center align-items-center" id="quote-box">
+            <blockquote class="blockquote text-center p-5">
+              <p class="mb-0 text-justify" id="text">Place for quotes</p>
+              <footer class="blockquote-footer"><cite title="Author" id="author">Place for author</cite></footer>
+              </blockquote>
+              <button class="btn" id="new-quote">New Quote</button>
+              <a href="twitter.com/intent/tweet" id="tweet-quote">Tweet it!</a>
+    </main>
   );
 }
 
