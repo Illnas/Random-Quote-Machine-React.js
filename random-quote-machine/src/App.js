81b07d2fcf46f8df1424992648bc969f0e4f5b98
Nadatari <dariosting@gmail.com>
2021-06-10 10:15:19 +0200

    Project is finished and everything is working cleanly

--- random-quote-machine/src/App.js
+++ random-quote-machine/src/App.js
@@ -1,19 +1,42 @@
-import Quote from "./assets/components/Quote"
-import Author from "./assets/components/Author"
-import Rng from "./assets/components/Rng"
-
+import { useState } from "react";
+import { useEffect } from "react";
+const fetchURL = "https://type.fit/api/quotes";
 
 
 
 function App() {
+  const [data, setData] = useState();
+  const [author, setAuthor] = useState('Place for Author');
+  const [text, setText] = useState('Place for quote');
+
+  const getData = () =>
+    fetch(fetchURL).then((res) => res.json())
+
+  useEffect(() => {
+    getData().then((data) => setData(data))
+  }, [])
+
+  function onClick() {
+    let rng = Math.floor(Math.random() * 1643);
+    //console.log(rng)
+    //console.log(data)
+    setAuthor(data[rng].author);
+    setText(data[rng].text);
+
+  }
+
   return (
-    <main  className="container" id="quote-box">
-      <blockquote>
-          <Quote />
-          <Author />
-      </blockquote>
-          
-          <Rng />
+    <main className="container" id="quote-box">
+      <div className="quote-container">
+      <p id="text">{text}</p>
+      <cite id="author">'{author !== null ? author : "Author Unknown"}'</cite>
+      </div>
+      <button className="btn" id="new-quote" onClick = {onClick}>
+        New Quote
+      </button>
+      <a href="twitter.com/intent/tweet" id="tweet-quote" target="_blank">
+        Tweet this quote
+      </a>
     </main>
   );
 }
